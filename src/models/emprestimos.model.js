606ab0c539551537c6a190c40014e2143a16b371
danielfernandoaraujo <danielferna11@gmail.com>
2022-06-13 16:48:37 -0300

    fix: erro no type Date

--- src/models/emprestimos.model.js
+++ src/models/emprestimos.model.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const DataSchema = new mongoose.Schema({
     nome_aluno: String,
     nome_livro: String,
-    data_prazo: String,
-    data_atual: String
+    data_prazo: Date,
+    data_atual: Date
 })
 
 const emprestimos = mongoose.model ( 'emprestimos' , DataSchema );
